28d718e1eb08028fc1bf9dbc8312acae78953999
Aleksandrs Pirags <alpibit@hotmail.com>
2024-03-19 21:33:02 +0000

    Merge

---
+++ utils/get-uploads.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../config/autoload.php';
+
+$db = new Database();
+$conn = $db->connect();
+
+$upload = new Upload($conn);
+$files = $upload->listFiles();
+
+$response = ['uploads' => []];
+
+foreach ($files as $file) {
+    $response['uploads'][] = ['url' => $file['url']];
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
